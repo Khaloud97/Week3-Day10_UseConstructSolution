052080ee25bf89767d6c43b745a3e4212d83405b
Khaloud <hamedkhaloud71@gmail.com>
2023-11-28 14:37:44 +0400

    Add project files.

---
+++ Week3-Day10_UseConstruct/Product.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Week3_Day10_UseConstruct
+{
+    class Product
+    {
+        public const double py = 3.14;
+        public static int ProductId; //this is static (fixed vale) is class 
+        public int Id;
+        public string Name;
+        public string Description;
+        public double Price;
+
+
+        public Product(string Name, string Description, double Price)
+        {
+            ProductId += 1;
+            this.Id = ProductId;
+            this.Name = Name;
+            this.Description = Description;
+            this.Price = Price;
+
+        }
+        
+
+    }
+}
---
+++ Week3-Day10_UseConstruct/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Week3_Day10_UseConstruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, to Proudect system!");
+
+            Product p1 =new Product("Mobile","mobile with red color",210.3);
+            Product p2 = new Product("Lab", "Lab with blue color", 300.2);
+            Product p3 = new Product("Phone", "Phone with blue color", 100.5);
+
+            Console.WriteLine("Py = "+ Product.py);//call const  (<className> . <variableName>);
+            
+            Console.WriteLine($" {p1.Id}, {p1.Name} , {p1.Description} ,price of it= {p1.Price}");
+            Console.WriteLine($" {p2.Id}, {p2.Name} , {p2.Description} ,price of it= {p2.Price}");
+            Console.WriteLine($" {p3.Id}, {p3.Name} , {p3.Description} ,price of it= {p3.Price}");
+        }
+    }
+}
